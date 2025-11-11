c25e580ca98911209a14397d1ae4785234551146
Pyips <Git@Pyips.dev>
2025-11-11 16:48:30 -0600

    Add ternary tutorial

---
+++ ternary/index.js
@@ -0,0 +1,23 @@
+// Pyips.dev
+
+/*
+let age = 21;
+let message = age >= 18 ? "You're an adult" : "You're a kiddo";
+console.log(message);
+*/
+/*
+let time = 9;
+let greeting = time < 12 ? "Good Morning!" : "Good Afternoon!";
+console.log(greeting);
+*/
+/*
+let isStudent = false;
+let message = isStudent ? "You're a student" : "You're not a student";
+console.log(message);
+*/
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(
+  `Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}`
+);
