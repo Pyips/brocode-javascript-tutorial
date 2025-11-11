3a3bd06f692d383cdc88cf5ae9563d427f0e38d3
Pyips <Git@Pyips.dev>
2025-11-11 12:07:48 -0600

    Add Variables tutorial

---
+++ variables/index.js
@@ -0,0 +1,44 @@
+// Pyips.dev
+
+
+let fullName ="Pyips Dev";
+let age ="25";
+let student ="false";
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Student status: ${student}`;
+
+/*
+
+console.log (`--------------`);
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`You are online: ${online}`);
+console.log(`You are for sale: ${online}`);
+console.log(`Student status: ${online}`);
+
+console.log (`--------------`);
+
+let firstName ="Bro";
+let favoriteFood = "pizza";
+let email = "fake@pyips.dev";
+
+console.log(`Your name is ${firstName}, your like ${favoriteFood}, and your email is ${email}`);
+
+console.log (`--------------`);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(age);
+console.log(price);
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is: ${gpa}`)
+
+*/
