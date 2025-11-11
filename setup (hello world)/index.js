cc4808a203157b5bba1c45cea893a86e13e2c2d9
Pyips <Git@Pyips.dev>
2025-11-11 11:42:30 -0600

    Add setup tutorial

---
+++ setup (hello world)/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello, World!`);
+console.log(`I like pizza!`);
+
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello, World!`;
+document.getElementById("myP").textContent = `I like pizza!`;
+
+// This is a comment
+
+/* 
+    This
+    is
+    a
+    comment
+*/
