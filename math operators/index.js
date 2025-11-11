69221170ac09b03db022d427a00d8c8402c9cf85
Pyips <Git@Pyips.dev>
2025-11-11 13:19:55 -0600

    Add math operators tutorial

---
+++ math operators/index.js
@@ -0,0 +1,30 @@
+// Pyips.dev
+
+// let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+// let extraStudents = students % 2;
+
+//students += 1;
+//students -= 1;
+//students *= 1;
+//students /= 1;
+//students **= 2;
+//students %= 2;
+
+//students++;
+//students--;
+
+//console.log(students);
+
+//let result = 1 + 2 * 3 + 4 ** 2;
+
+//let result = (12 % 5) + 8 / 2;
+
+let result = 6 / 2 ** 7;
+
+console.log(result);
