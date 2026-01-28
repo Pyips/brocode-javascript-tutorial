43ea1b25b8b2901bb0024af5bd159a20233be28d
Pyips <Git@Pyips.dev>
2026-01-28 08:52:04 -0600

    Add logical operators tutorial

---
+++ logical operators/index.js
@@ -0,0 +1,31 @@
+// Pyips.dev
+
+// AND operator
+
+// const temp = 25;
+
+// if (temp > 0 && temp <= 30) {
+//   console.log("The weather is GOOD");
+// } else {
+//   console.log("The weather is BAD");
+// }
+
+// OR operator
+
+// const temp = 25;
+
+// if (temp <= 0 || temp > 30) {
+//   console.log("The weather is BAD");
+// } else {
+//   console.log("The weather is GOOD");
+// }
+
+// NOT operator
+
+const isSunny = true;
+
+if (!isSunny) {
+  console.log("It is CLOUDY");
+} else {
+  console.log("It is SUNNY");
+}
