fb8618b0efbed070e151bda9f717ac3ef694429f
Pyips <Git@Pyips.dev>
2026-01-28 10:42:54 -0600

    Add for loops tutorial

---
+++ for loops/index.js
@@ -0,0 +1,16 @@
+// Pyips.dev
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// console.log("Happy New Year!");
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    // continue; skips the number
+    break; // stops at the number
+  } else {
+    console.log(i);
+  }
+}
