a10b140f1ce8ee71ac1b9ff22da7d9b6d5df9fd7
Pyips <Git@Pyips.dev>
2026-02-02 11:43:07 -0600

    Add arrays tutorial

---
+++ arrays/index.js
@@ -0,0 +1,35 @@
+// Pyips.dev
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+// fruits[3] = "coconut";
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("coconut");
+// let index = fruits.indexOf("mango");
+
+// console.log(numOfFruits);
+// console.log(index);
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// for (let i = fruits.length - 1; i >= 0; i--) {
+//   console.log(fruits[i]);
+// }
+
+// fruits.sort();
+fruits.sort().reverse();
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
